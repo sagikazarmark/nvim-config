bd37a5280be3c2f1b07cecdb4d09f9b34afffa74
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-03-22 10:32:21 +0100

    chore: disable conceal on markdown
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- https://github.com/LazyVim/LazyVim/discussions/269#discussioncomment-4981602
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown" },
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
+})
