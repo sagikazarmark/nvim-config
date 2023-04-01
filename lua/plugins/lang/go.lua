1aef83441eef7dc44598c9af8cff97f3335bc8c7
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-04-01 18:41:41 +0200

    feat: configure go.nvim
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- lua/plugins/lang/go.lua
+++ lua/plugins/lang/go.lua
@@ -9,4 +9,17 @@ return {
       },
     },
   },
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+  },
 }
