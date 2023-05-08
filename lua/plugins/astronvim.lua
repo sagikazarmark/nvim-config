c9131595ee7423c657180ecd3d2fb77aec5b831c
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-05-08 15:32:09 -0700

    update dependencies and install missing astronvim
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- lua/plugins/astronvim.lua
+++ lua/plugins/astronvim.lua
@@ -1,6 +1,7 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  "AstroNvim/AstroNvim",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
