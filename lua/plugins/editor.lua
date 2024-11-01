39ee1cbe5ea2babf880863a294926d9daa8e5b1d
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-11-01 20:49:28 +0100

    updates
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,6 @@
+return {
+  { import = "lazyvim.plugins.editor.illuminate" },
+  { import = "lazyvim.plugins.editor.leap" },
+  { import = "lazyvim.plugins.editor.mini-move" },
+  { import = "lazyvim.plugins.editor.refactoring" },
+}
