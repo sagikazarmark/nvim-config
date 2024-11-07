a71554634c48aaf440d2bf9081abf0615b072d9b
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-11-07 15:52:17 +0100

    fix telescope
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -3,4 +3,25 @@ return {
   { import = "lazyvim.plugins.extras.editor.leap" },
   { import = "lazyvim.plugins.extras.editor.mini-move" },
   { import = "lazyvim.plugins.extras.editor.refactoring" },
+  { import = "lazyvim.plugins.extras.editor.telescope" },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      local _, actions = pcall(require, "telescope.actions")
+      opts.defaults.mappings = {
+        i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<C-n>"] = actions.cycle_history_next,
+          ["<C-p>"] = actions.cycle_history_prev,
+        },
+        -- for normal mode
+        n = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+        },
+      }
+    end,
+  },
 }
